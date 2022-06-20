35cee8d94768b8ca7408743f03594285b835da5b
Евгений <msk9199625687@yandex.ru>
2022-06-20 17:50:47 +0300

    add url and module3

---
+++ doc/url.js
@@ -0,0 +1,4 @@
+import 'https://jakearchibald.com/utils/bar.js'; // URL
+import '/utils/bar.js'; // Абсолютный путь относительно домена
+import './bar.js'; // Относительный путь от текущего модуля
+import '../bar.js'; // Относительный путь от родительской директории
