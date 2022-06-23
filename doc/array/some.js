79c61a313bc555c9afca4e3c327681461a35d5b2
Евгений <msk9199625687@yandex.ru>
2022-06-23 10:59:43 +0300

    add template

--- doc/array/some.js
+++ doc/array/some.js
@@ -1,4 +1,4 @@
-возвращает если что-то имеет тру
+// возвращает если что-то имеет тру
 
 
 const task = {
@@ -21,3 +21,19 @@ const isTaskRepeating = (repeating) => {
 // мотед every протовиположный
 
 // predicat: return true or false
+
+const numbers = [1, 4, 10, 5];
+
+const isExistsOverFive = numbers.some((value) => {
+  return value > 5; // Проверяем каждый элемент, больше ли он, чем 5
+}); // Когда some дойдёт до 10, то прекратит работу и вернёт true
+
+const isExistsOverTwenty = numbers.some((value) => {
+  return value > 20; // Проверяем каждый элемент, больше ли он, чем 20
+}); // some пройдёт все элементы, они все меньше 20, поэтому some вернёт false
+
+// У метода .some() есть одна особенность. Если вызвать его на пустом массиве, то результатом всегда будет false вне зависимости от условия:
+
+const result = [].some(() => {
+  return 1 === 1;
+}); // так как массив пустой, some вернёт false
